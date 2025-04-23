a0a96a3675432c1767ee457de8e9b40f9a965c95
William Suarez <147040637+Suarwill@users.noreply.github.com>
2025-04-23 17:42:45 -0400

    Update 3habilitarPuerto80.sh

--- front-inventario/3habilitarPuerto80.sh
+++ front-inventario/3habilitarPuerto80.sh
@@ -3,6 +3,8 @@ set -e
 
 echo "🔐 Habilitando puerto 80 en el firewall..."
 sudo dnf install firewalld -y
+sudo systemctl start firewalld
+sudo systemctl enable firewalld
 sudo firewall-cmd --permanent --add-port=80/tcp
 sudo firewall-cmd --reload
 
