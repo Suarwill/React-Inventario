b0e30b3faba3ae0375c16d7b9531d0a4ebc31122
Your Name <you@example.com>
2025-04-23 17:15:37 -0400

    automatizando

--- front-inventario/1actualizador.sh
+++ front-inventario/1actualizador.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 
+# Actualiza paquetes
+sudo dnf update -y
+
+# Instala Node.js (usaremos Node 18 como ejemplo)
+curl -fsSL https://rpm.nodesource.com/setup_18.x | sudo bash -
+sudo dnf install -y nodejs git
+
+node -v
+npm -v
+
 echo "📁 Copiando archivos de build a /var/www/html"
 sudo rm -rf /var/www/html/*
 sudo cp -r ./front-inventario/build/* /var/www/html/
