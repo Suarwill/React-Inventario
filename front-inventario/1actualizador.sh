3defbe7b0b97e4400021df01dd8ad846572e5fe6
William Suarez <147040637+Suarwill@users.noreply.github.com>
2025-04-23 22:02:40 -0400

    Actualizar 1actualizador.sh

--- front-inventario/1actualizador.sh
+++ front-inventario/1actualizador.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 set -e
 
-# Actualiza paquetes
-sudo dnf update -y
-
-# Instala Node.js (usaremos Node 18 como ejemplo)
-curl -fsSL https://rpm.nodesource.com/setup_18.x | sudo bash -
-sudo dnf install -y nodejs git
-
-node -v
-npm -v
-
 echo "📁 Copiando archivos de build a /var/www/html"
 sudo mkdir -p /var/www/html
 sudo rm -rf /var/www/html/*
