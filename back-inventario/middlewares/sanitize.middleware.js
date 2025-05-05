a338be15e3e789a48ff42fbeb7d831653e1b2b7a
Suarwill <23763487k@gmail.com>
2025-05-05 18:16:01 -0400

    backend-middleware

--- back-inventario/middlewares/sanitize.middleware.js
+++ back-inventario/middlewares/sanitize.middleware.js
@@ -1,19 +1,21 @@
 const sanitizeInput = (req, res, next) => {
-    const sanitize = (str) => {
-      if (typeof str !== 'string') return str;
-      return str
-        .replace(/['"`\\;]/, '')     // quitar comillas, backslash y punto y coma
-        .replace(/[<>]/, '')         // quitar tags HTML (prevención XSS básica)
-        .trim();
-    };
-  
-    // Revisa y limpia campos comunes
-    if (req.body.username) req.body.username = sanitize(req.body.username);
-    if (req.body.password) req.body.password = sanitize(req.body.password);
-    if (req.query.username) req.query.username = sanitize(req.query.username);
-    if (req.params.username) req.params.username = sanitize(req.params.username);
-  
-    next();
+  const sanitize = (str) => {
+    if (typeof str !== 'string') return str;
+    return str
+      .replace(/['"`\\;]/g, '') // eliminar comillas, backslash, punto y coma
+      .replace(/[<>]/g, '')     // eliminar etiquetas HTML
+      .trim();
   };
+
+  // Comprobaciones defensivas antes de acceder a propiedades
+  if (req.body && req.body.username) req.body.username = sanitize(req.body.username);
+  if (req.body && req.body.password) req.body.password = sanitize(req.body.password);
   
-  module.exports = sanitizeInput;  
+  if (req.query && req.query.username) req.query.username = sanitize(req.query.username);
+  
+  if (req.params && req.params.username) req.params.username = sanitize(req.params.username);
+
+  next();
+};
+
+module.exports = sanitizeInput;
