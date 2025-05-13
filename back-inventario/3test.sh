1c09e895ca991b4e0f9b1679fc169ab6073fe525
Suarwill <23763487k@gmail.com>
2025-05-13 17:46:02 -0400

    backend

--- back-inventario/3test.sh
+++ back-inventario/3test.sh
@@ -2,11 +2,11 @@
 
 # prueba de crear usuario
 echo "Realizando prueba de login de usuario..."
-curl -X POST http://localhost:3000/user/login \
+curl -X POST http://localhost:3000/api/user/login \
     -H "Content-Type: application/json" \
     -d '{"username":"admin", "password":"1234"}'
 
 # prueba de busqueda
 echo "Realizando prueba de búsqueda de usuario..."
-curl -X GET http://localhost:3000/user/search?username=admin \
+curl -X GET http://localhost:3000/api/user/search?username=admin \
     -H "Content-Type: application/json"
