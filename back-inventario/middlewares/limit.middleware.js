ed3db810f9f0c86d7fa46a2de17326b83d2ccf08
Suarwill <23763487k@gmail.com>
2025-05-29 23:50:19 -0400

    front inventario

--- back-inventario/middlewares/limit.middleware.js
+++ back-inventario/middlewares/limit.middleware.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
     windowMs: 1 * 60 * 1000, // 1 minuto
-    max: 200, // Máximo de 100 solicitudes por IP
+    max: 100, // Máximo de 100 solicitudes por IP
     message: 'Demasiadas solicitudes desde esta IP, por favor inténtelo más tarde.',
 });
 
