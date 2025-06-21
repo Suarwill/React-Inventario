eca5663c7dcbfc251a0fac92a0a65f85e7cafa43
Suarwill <23763487k@gmail.com>
2025-06-21 01:04:47 -0400

    front inventario

--- src/components/Zonal/zonal.js
+++ src/components/Zonal/zonal.js
@@ -9,10 +9,36 @@ const Zonal = () => {
         <div className="zonal-container">
             <Navbar sector={sector}/>  
             <div className="zonal-header">
-                <h1>Panel de </h1>
+                <button className="main-button-z"> Verificaciones </button>
+                <button className="main-button-z"> Inventarios </button>
+                <button className="main-button-z"> Depositos </button>
+                <button className="main-button-z"> Tareas Pendientes </button>
             </div>
             <div className="zonal-content">
-                <h2>Bienvenido al Panel Zonal</h2>
+                <div className='display-izq'>
+                    <h3> Cumplimiento de Verificaciones </h3>
+
+                </div>
+                <div className='display-central'>
+                    <div className='box-central-superior'>
+                        <div className='box-finance'>
+                            <h3> Depósitos </h3>
+
+                        </div>
+                        <div className='box-inventory'>
+                            <h3> Inventarios </h3>
+                            
+                        </div>
+                    </div>
+                    <div className='box-tareas'>
+                        <h3> Tareas Pendientes </h3>
+                    </div>
+                </div>
+                <div className='display-der'>
+                    <h3> Mapa de módulos </h3>
+
+                </div>
+
             </div>
         </div>
     );
