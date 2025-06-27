2055522472d97fe96c9f2e25d21f7a4be7e99d61
Suarwill <23763487k@gmail.com>
2025-06-27 15:13:33 -0400

    reestructurando

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/Login/Login'; // Ruta permanece igual
+import Login from './pages/Login/Login'; // Ruta permanece igual
 import Dashboard from './pages/Dashboard/Dashboard'; // Actualización de la ruta
 import AdminPanel from './pages/AdminPanel/AdminPanel'; // Actualización de la ruta
 import Zonal from './pages/Zonal/zonal'; // Actualización de la ruta
